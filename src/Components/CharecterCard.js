439a92676dcb64e8cfeee9c4aba09c10b382f45a
saikat1095 <mdabutahersaikat@gmail.com>
2021-08-20 00:04:15 +0600

    Pulling Charecters form graphql api id done and pagination applied

--- src/Components/CharecterCard.js
+++ src/Components/CharecterCard.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import './CharecterCard.scss'
 
-const CharecterCard = ({characters}) => {
+const CharecterCard = ({charecter}) => {
+    console.log(charecter);
     return (
-        <li className="card-list">
-            console.log(characters)
-            <a href="" className="card">
-                <img src="https://i.imgur.com/oYiTqum.jpg" className="cardImage" alt="" />
-                <div className="cardOverlay">
-                    <div className="cardHeader">                   
-                        <div className="cardHeader-text">
-                            <h3 className="cardTitle">Jessica Parker</h3>            
-                            <span className="cardStatus">1 hour ago</span>
-                        </div>
-                    </div>
-                    <p className="cardDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-                </div>
-            </a>      
-        </li>   
+        <>
+            {charecter.map((char) => {
+                return(
+                    <li className="card-list" key={char.id}>
+                        {/* {console.log(charecter)} */}
+                        <a href="" className="card">
+                            <img src={char.image} className="cardImage" alt="" />
+                            <div className="cardOverlay">
+                                <div className="cardHeader">                   
+                                    <div className="cardHeader-text">
+                                        <h3 className="cardTitle">{char.name}</h3>            
+                                        <span>{char.gender}</span>
+                                        <span>{char.species}</span>
+                                        <span>{char.status}</span>
+                                    </div>
+                                </div>
+                                <p className="cardDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>      
+                    </li>   
+                )
+            })}
+            <h1>no data here</h1>
+        </>
 
     )
 }
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,46 +1,59 @@
-import React,{useEffect} from 'react';
+import React,{useState, useEffect} from 'react';
 import {useLazyQuery} from '@apollo/client'
 import { GET_ALL_CHARACTERS} from '../GraphQl/Query'
 import CharecterCard from '../Components/CharecterCard';
 import './HomeScreen.scss';
-import { Spin } from 'antd';
+import { Spin , Pagination , BackTop } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
 const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
 
 const HomeScreen = () => {
-    // const [citySearched, setcitySearched] = useState("")
-    
+    const [charecter, setCharecter] = useState(null)
+    const [page, setPage] = useState(1);
     const [getCharecters, {loading, error , data}] = useLazyQuery(GET_ALL_CHARACTERS, {
-        variables : { page : 1}
+        variables : { page : page}
     })
-    
-    // useEffect(()=>{
-    //     if(data){
-    //         getCharecters();
-    //         console.log(data);
-    //     }
-    // },[data])
 
+    useEffect(() => {
+        if(data){
+            setCharecter(data?.characters.results)
+        }else{
+            getCharecters();
+        }
+    }, [data , charecter , page])
+    
+  
     if(error) return <h1>Error Found</h1>
     // if(data) {
     //     console.log(data)
     // }
 
-    useEffect(() => {
-        getCharecters()
-    }, [])
+
+    const handlePaginationChange = (e) => {
+        console.log(e);
+        setPage(e)
+    }
+
+ 
 
 
     return (
         <div className="charecterCard">
-            {loading ? <Spin indicator={antIcon} /> : (
+            {loading ? <Spin indicator={antIcon} /> : ""}
+            {charecter ? (
                 <>
                     <ul className="cards">
-                        <CharecterCard data={data} />   
+                        <CharecterCard charecter={charecter} />   
                     </ul>
                 </>
+            ) : (
+                <h1>No data Available</h1>
             )}
+            <div className="pagination">
+                <Pagination onChange={handlePaginationChange} defaultCurrent={1} total={50} /> 
+            </div>
+            <BackTop />
         </div>
     )
 }
